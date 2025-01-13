6e86a729c02186ac56975cd4cb471bb0e15acc6c
Eliska Knop Kostkova <e.knopkostkova@gmail.com>
2025-01-13 18:01:32 +0100

    data.js and adding React Router

---
+++ ikea-vs-death-metal/src/data/data.js
@@ -0,0 +1,113 @@
+const data = [
+    {
+      name: "Kallax",
+      type: "Ikea",
+      image: "",
+    },
+    {
+      name: "Behemoth",
+      type: "Metal Band",
+      image: "",
+    },
+    {
+        name: "Malm",
+        type: "Ikea",
+        image: "",
+      },
+      {
+        name: "Draconian",
+        type: "Metal Band",
+        image: "behemoth.jpg",
+      },
+  ];
+  
+  export default data;
+
+
+
+
+// const data = [
+//     {
+//       name: "",
+//       type: "Ikea",
+//       image: "",
+//     },
+//     {
+//         name: "",
+//         type: "Ikea",
+//         image: "",
+//       },
+//     {
+//         name: "",
+//         type: "Ikea",
+//         image: "",
+//       },
+//     {
+//         name: "",
+//         type: "Ikea",
+//         image: "",
+//       },
+//     {
+//         name: "",
+//         type: "Ikea",
+//         image: "",
+//       },
+//     {
+//         name: "",
+//         type: "Ikea",
+//         image: "",
+//       },
+//     {
+//         name: "",
+//         type: "Ikea",
+//         image: "",
+//       },
+
+//     {
+//       name: "",
+//       type: "Band",
+//       image: "",
+//         },
+//     {
+//         name: "",
+//         type: "Band",
+//         image: "",
+//       },
+//     {
+//         name: "",
+//         type: "Band",
+//         image: "",
+//       },
+//       {
+//         name: "",
+//         type: "Band",
+//         image: "",
+//       },
+//     {
+//         name: "",
+//         type: "Band",
+//         image: "",
+//       },
+//       {
+//         name: "",
+//         type: "Band",
+//         image: "",
+//       },
+//     {
+//         name: "",
+//         type: "Band",
+//         image: "",
+//       },
+//       {
+//         name: "",
+//         type: "Band",
+//         image: "",
+//       },
+//     {
+//         name: "",
+//         type: "Band",
+//         image: "",
+//       },
+//   ];
+  
+//   export default data;
