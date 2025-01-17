eb444165c89844124bea52e6f929e1d5578882bf
Eliska Knop Kostkova <e.knopkostkova@gmail.com>
2025-01-17 03:20:58 +0100

    attempt for the shuffled array logic

--- ikea-vs-death-metal/src/pages/Game.jsx
+++ ikea-vs-death-metal/src/pages/Game.jsx
@@ -1,9 +1,19 @@
 import { useNavigate } from "react-router-dom"
+import { useState, useEffect } from "react"
 import data from "../data/data"
+
+function shuffleArray() {
+  const shuffledArray = [];
+  for (let i = shuffledArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+  }
+  return shuffledArray;
+}
 
 export default function Game() {
 
-  console.log(data)
+  // console.log(data)
 
     // const names = data.map(item => item)
     // console.log(names)
@@ -14,22 +24,51 @@ export default function Game() {
     // const lala = Math.floor(Math.random() * data.length)
     // console.log(lala)
 
-    const oneObject = data[Math.floor(Math.random() * data.length)]
-    console.log(oneObject)
+
+    
+    const newDataArray = shuffleArray(data)
+    console.log(newDataArray)
+
+    const [remainingItems, setRemainingItems] = useState(newDataArray)
+    
+    const [currentItem, setCurrentItem] = useState(null)
+    
+    useEffect(() => {
+      if (remainingItems.length > 0) { 
+        setCurrentItem(remainingItems[0])
+      }
+    }, [remainingItems])
+    
+
+    // const oneObject = data[Math.floor(Math.random() * data.length)]
+    // console.log(oneObject)
 
     const navigate = useNavigate()
 
+    
+    const updatedItems = remainingItems.slice(1)
+    
+    if (updatedItems.length === 0) {
+      alert("Game Over! You've played all items.")
+      setCurrentItem(null)
+    } else {
+      setRemainingItems(updatedItems);
+      setCurrentItem(updatedItems[0]);
+      console.log("current item is " + currentItem)
+    }
+    
+    
     const handleIkeaBag = () => {
-      oneObject.band ? navigate("/bandFail") : navigate("/ikeaCorrect")
+      currentItem.band ? navigate("/bandFail") : navigate("/ikeaCorrect")
     }
 
     const handleMetalBtn = () => {
-      oneObject.band ? navigate("/bandCorrect") : navigate("/ikeaFail")
+      currentItem.band ? navigate("/bandCorrect") : navigate("/ikeaFail")
     }
 
     return (
         <div>
-          <h1>{oneObject.name}</h1>
+          {/* <h1>{currentItem.name}</h1> */}
           <button onClick={handleIkeaBag}>
             <img src="/images/71cQiE5yR2L._AC_SL1239_.jpg" alt="" width ="200"/>
           </button>
@@ -40,4 +79,6 @@ export default function Game() {
         </div>
       );
 }
+
+// console.log("remainingItems are: " + remainingItems)
 
