1833611bcbb248846b00dccda9ecfa00615434c1
Eliska Knop Kostkova <e.knopkostkova@gmail.com>
2025-01-19 23:23:25 +0100

    total mess

--- ikea-vs-death-metal/src/pages/Game.jsx
+++ ikea-vs-death-metal/src/pages/Game.jsx
@@ -2,8 +2,8 @@ import { useNavigate } from "react-router-dom"
 import { useState, useEffect } from "react"
 import data from "../data/data"
 
-function shuffleArray() {
-  const shuffledArray = [];
+function shuffleArray(arr) {
+  const shuffledArray = [...arr];
   for (let i = shuffledArray.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
@@ -11,8 +11,12 @@ function shuffleArray() {
   return shuffledArray;
 }
 
+console.log("hello")
+
+
 export default function Game() {
 
+  const navigate = useNavigate()
   // console.log(data)
 
     // const names = data.map(item => item)
@@ -27,48 +31,66 @@ export default function Game() {
 
     
     const newDataArray = shuffleArray(data)
-    console.log(newDataArray)
 
-    const [remainingItems, setRemainingItems] = useState(newDataArray)
+    console.log("shuffled array is: ", newDataArray)
+
+    const [remainingItems, setRemainingItems] = useState(() => shuffleArray(data))
+
+    console.log("remainingItems are:", remainingItems)
+
     
-    const [currentItem, setCurrentItem] = useState(null)
+    const [currentItem, setCurrentItem] = useState(remainingItems[0])
+
+    console.log("currentItem are: ", currentItem)
+
+    
+    
     
     useEffect(() => {
       if (remainingItems.length > 0) { 
         setCurrentItem(remainingItems[0])
+      } else {
+        setCurrentItem(null); // Set to null if no items remain
       }
     }, [remainingItems])
     
+    console.log("remainingItems after use effect are:", remainingItems)
 
+    
+    
     // const oneObject = data[Math.floor(Math.random() * data.length)]
     // console.log(oneObject)
-
-    const navigate = useNavigate()
-
     
-    const updatedItems = remainingItems.slice(1)
-    
-    if (updatedItems.length === 0) {
-      alert("Game Over! You've played all items.")
-      setCurrentItem(null)
-    } else {
-      setRemainingItems(updatedItems);
-      setCurrentItem(updatedItems[0]);
-      console.log("current item is " + currentItem)
-    }
+
+    // if (remainingItems.length === 0) {
+    //   alert("Game Over! You've played all items.")
+    //   setCurrentItem(null)
+    // } else {
+    //   setRemainingItems(updatedItems);
+    //   setCurrentItem(updatedItems[0]);
+    //   console.log("current item is " + currentItem.name)
+    // }
     
     
     const handleIkeaBag = () => {
       currentItem.band ? navigate("/bandFail") : navigate("/ikeaCorrect")
     }
-
+    
     const handleMetalBtn = () => {
       currentItem.band ? navigate("/bandCorrect") : navigate("/ikeaFail")
     }
+    
+    // const updatedItems = remainingItems.slice(1)
+    // setRemainingItems(updatedItems);
+
+    // if (updatedItems.length === 0) {
+    //   alert("Game Over! You've played all items.")
+    //   // setCurrentItem(null)
+    // }
 
     return (
         <div>
-          {/* <h1>{currentItem.name}</h1> */}
+          <h1>{currentItem.name}</h1>
           <button onClick={handleIkeaBag}>
             <img src="/images/71cQiE5yR2L._AC_SL1239_.jpg" alt="" width ="200"/>
           </button>
