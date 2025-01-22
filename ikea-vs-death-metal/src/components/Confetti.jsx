a107b622c50f93b2f66bdd5b68fb4f1cea6a799a
Eliska Knop Kostkova <e.knopkostkova@gmail.com>
2025-01-22 23:45:35 +0100

    confetti window size fixed

--- ikea-vs-death-metal/src/components/Confetti.jsx
+++ ikea-vs-death-metal/src/components/Confetti.jsx
@@ -3,9 +3,28 @@ import { useEffect, useState } from "react"
 import ReactConfetti from "react-confetti"
 
 export default function Confetti() {
+    
+    
+    const [windowDimension, setWindowDimension] = useState({width: window.innerWidth, height: window.innerHeight})
+
+    const detectSize = () => {
+        setWindowDimension({width: window.innerWidth, height: window.innerHeight})
+    }
+
+    useEffect(() => {
+        window.addEventListener('resize', detectSize)
+        return () => {
+            window.removeEventListener('resize', detectSize)
+        }
+    }, [windowDimension])
+
+
     return (
         <div>
-            <ReactConfetti />
+            <ReactConfetti 
+                width={windowDimension.width}
+                height={windowDimension.height}
+            />
         </div>
     )
 }
