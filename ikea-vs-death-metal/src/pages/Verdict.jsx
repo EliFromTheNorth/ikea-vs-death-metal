9fdaabe3f41aaaab6c426f903634a6bd7eb12aae
Eliska Knop Kostkova <e.knopkostkova@gmail.com>
2025-01-25 21:45:05 +0100

    white gap kinda fixed

--- ikea-vs-death-metal/src/pages/Verdict.jsx
+++ ikea-vs-death-metal/src/pages/Verdict.jsx
@@ -44,14 +44,14 @@ export default function Verdict({state}) {
     
 
     return (
-        <>
+        <div>
           <h1>{correctAnswer ? "Hell YES!" : "Ouch"}</h1>
           <h2> {currentItem.name} is {currentItem.band ? "a band" : "an ikea product"}!</h2>
           <br />
           {currentItem.band ? <VerdictVideo /> : <VerdictImage />}
           <br />
-          <button onClick={handleNext} >Next</button>
-         </> 
+          <button className="btn-next" onClick={handleNext}>Next</button>
+         </div> 
     )
 
 }
