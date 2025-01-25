ac69c68e22fe4cee137242e74506ec5455fcc1c1
Eliska Knop Kostkova <e.knopkostkova@gmail.com>
2025-01-25 20:13:59 +0100

    Title component replaced

--- ikea-vs-death-metal/src/pages/Verdict.jsx
+++ ikea-vs-death-metal/src/pages/Verdict.jsx
@@ -12,7 +12,7 @@ export default function Verdict({state}) {
 
         // console.log("Updated Remaining Items:", updatedItems)
 
-        const updatedScore = ((currentItem.band && buttonClicked === "bandBtn") || (!currentItem.band && buttonClicked === "ikeaBtn")) ? score + 1 : score
+        // const updatedScore = ((currentItem.band && buttonClicked === "bandBtn") || (!currentItem.band && buttonClicked === "ikeaBtn")) ? score + 1 : score
         
         if (updatedItems.length > 0) {
             navigate("/game", { state: { updatedItems: updatedItems, updatedScore: updatedScore  } })
@@ -24,12 +24,29 @@ export default function Verdict({state}) {
         //  console.log("updates score is: ", updatedScore)
     }
 
+    //***************************************** */
+
+    let correctAnswer = null
+
+    if ((currentItem.band && buttonClicked === "ikeaBtn") || (!currentItem.band && buttonClicked === "bandBtn")) {
+      correctAnswer = false
+  } else if 
+      ((currentItem.band && buttonClicked === "bandBtn") || (!currentItem.band && buttonClicked === "ikeaBtn")) {
+        correctAnswer = true
+  }
+
+  const updatedScore = correctAnswer ? score + 1 : score
+
+
+    //***************************************** */
+
     
 
     return (
         <>
-          <Title />
-          <h1> {currentItem.name} is {currentItem.band ? "a band" : "an ikea product"}!</h1>
+          {/* <Title /> */}
+          <h1>{correctAnswer ? "Hell YES!" : "Ouch"}</h1>
+          <h2> {currentItem.name} is {currentItem.band ? "a band" : "an ikea product"}!</h2>
 
         {/* tady bude condicional statement ze bud component ikeaimage nebo component video, podle toho, esli je .band true */}
           <img src={currentItem.image} alt="" width="200" />
