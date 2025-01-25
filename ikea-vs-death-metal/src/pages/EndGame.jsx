2b2dee490e8a3d76c8d08f5b91bcb61a4d9cd031
Eliska Knop Kostkova <e.knopkostkova@gmail.com>
2025-01-25 22:07:40 +0100

    score evaluation - comment added

--- ikea-vs-death-metal/src/pages/EndGame.jsx
+++ ikea-vs-death-metal/src/pages/EndGame.jsx
@@ -17,10 +17,30 @@ export default function EndGame({state}) {
         navigate("/")
     }
 
+    let comment = ""
+    switch(true) {
+        case score < 4:
+            comment = "Omg, you should really visit Sweden"
+            break
+        case score < 8:
+            comment = "Getting there... but not quite there yet!"
+            break
+        case score < 12:
+            comment = "You’ve got the basics down, but there's room for more metal!"
+            break
+        case score < 15:
+            comment = "You're almost there... One with the metal gods!"
+            break;
+        default:
+            comment = "You're a true death metal fan! Headbang your way to glory!"
+            break
+    }
+
     return (
         <>
             <Confetti />
             <h1>Game Over</h1>
+            <h2>{comment}</h2>
             <h2>Your Score: {score}/{data.length}</h2>
             <button onClick={ handlePlayAgain }>Play again</button>
         </>
