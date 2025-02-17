1c302f1be2e6083b2f181489177d682a1328760e
Eliska Knop Kostkova <e.knopkostkova@gmail.com>
2025-02-17 17:07:44 +0100

    intro press Enter function

--- ikea-vs-death-metal/src/pages/Intro.jsx
+++ ikea-vs-death-metal/src/pages/Intro.jsx
@@ -1,19 +1,34 @@
-// import {useNavigate} from "react-router-dom"
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom"
 
 
 export default function Intro() {
-//   const navigate = useNavigate()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const handleKeyPress = (event) => {
+      if (event.key === 'Enter') {
+        navigate('/home');
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyPress);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyPress);
+    };
+  }, [navigate]);
   
     return (
       <div>
-        <div className="home-container">
+        {/* <div className="home-container"> */}
                 <p>Deep in the dark Scandinavian forest, where legends are forged in fire and ice, words take on a life of their own. Some echo through the abyss of death metal, others rest upon the altars of IKEA.
 
                     Fifteen titles will be revealed to you. Only by trusting your instincts can you uncover their true origins.
 
-                        But beware—appearances can be deceiving. Choose wisely.
+                        But beware—appearances can be deceiving. Choose wisely. Press ENTER to continue.
                 </p>
-          </div>
+          {/* </div> */}
         </div>
     )
 }
