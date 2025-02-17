1a63b1d80ad3c1a324b5535c76c443baee50286a
Eliska Knop Kostkova <e.knopkostkova@gmail.com>
2025-02-17 18:17:09 +0100

    animation kinda done, not perfect

--- ikea-vs-death-metal/src/pages/Intro.jsx
+++ ikea-vs-death-metal/src/pages/Intro.jsx
@@ -1,9 +1,8 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom"
-
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function Intro() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   useEffect(() => {
     const handleKeyPress = (event) => {
@@ -18,20 +17,25 @@ export default function Intro() {
       window.removeEventListener('keydown', handleKeyPress);
     };
   }, [navigate]);
+
+  const text = `Deep in the dark Scandinavian forest, where legends are forged in fire and ice, words take on a life of their own. Some echo through the abyss of death metal, others rest upon the altars of IKEA. Fifteen titles will be revealed to you. Only by trusting your instincts can you uncover their true origins. But beware—appearances can be deceiving. Choose wisely. Press ENTER to continue.`;
+
+  const wrapWordsInSpans = (text) => {
+    const words = text.split(' ');
+    return words.map((word, index) => (
+      <span key={index} style={{ animationDelay: `${index * 0.5}s` }}>
+        {word}{' '}
+      </span>
+    ));
+  };
+
   
-    return (
-      <div>
-        {/* <div className="home-container"> */}
-                <p>Deep in the dark Scandinavian forest, where legends are forged in fire and ice, words take on a life of their own. Some echo through the abyss of death metal, others rest upon the altars of IKEA.
 
-                    Fifteen titles will be revealed to you. Only by trusting your instincts can you uncover their true origins.
-
-                        But beware—appearances can be deceiving. Choose wisely. Press ENTER to continue.
-                </p>
-          {/* </div> */}
-        </div>
-    )
+  return (
+    <div className="intro-container">
+      <p className="intro-text">
+        {wrapWordsInSpans(text)}
+      </p>
+    </div>
+  );
 }
-
-
-
