886a34da59da2d7443da92169c1515a8bb9177cb
Eliska Knop Kostkova <e.knopkostkova@gmail.com>
2025-02-22 00:41:17 +0100

    fog overflow fixed

--- ikea-vs-death-metal/src/components/Fog.jsx
+++ ikea-vs-death-metal/src/components/Fog.jsx
@@ -4,7 +4,7 @@ import "../Fog.css";
 
 export default function Fog() {
     return (
-    <div>
+    <div className="fogwrapper">
         <div id="foglayer_01" className="fog">
             <div className="image01"></div>
             <div className="image02"></div>
